81dce01946962ae4ec9510249821cf79e6e2bd8c
Sergey K <2hotab2@gmail.com>
2017-12-24 23:39:30 +0200

    calculator

---
+++ P1/venv/P1/calculator.py
@@ -0,0 +1,15 @@
+x=float(input('x'))
+y=float(input('y'))
+oper=input('rule')
+result = None
+
+if oper == '+':
+   result=x+y
+elif oper == '-':
+   result=x-y
+else:
+   print('Not support rule')
+
+
+if result is not None:
+    print('result',result)
